944f4cfa93a4e97f48934d1c665296ef8ebd97bb
carodriguezp <crpzgz92@gmail.com>
2024-08-21 12:36:44 +0200

    routes implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import { Container } from "./App.style";
 
 function App() {
   return (
     <Container>
       <h1>Fruityvice</h1>
+
+      <Routes>
+        <Route path="/" element={<div>RandomFruit component</div>} />
+        <Route path="/allfruits" element={<div>FruitList component</div>} />
+        <Route
+          path="/allfruits/:idFruit"
+          element={<div>FruitDetail component</div>}
+        />
+      </Routes>
     </Container>
   );
 }
