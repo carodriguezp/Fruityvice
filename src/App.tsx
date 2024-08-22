7927a520207a759bcde5326e7dac056ef0b171e4
carodriguezp <crpzgz92@gmail.com>
2024-08-22 10:29:41 +0200

    api services

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { Container } from "./App.style";
 
+//import { getDataFromApi, getDataByIdFromApi } from "./services/api";
+
 function App() {
+  // const allFruits = getDataByIdFromApi(71);
+  // console.log("allFruits", allFruits);
   return (
     <Container>
       <h1>Fruityvice</h1>
 
       <Routes>
         <Route path="/" element={<div>RandomFruit component</div>} />
-        <Route path="/fruits" element={<div>FruitList component</div>} />
+        <Route path="/fruits" element={<div>FruitList component </div>} />
         <Route path="/fruits/:id" element={<div>FruitDetail component</div>} />
       </Routes>
     </Container>
---
+++ src/services/api.ts
@@ -0,0 +1,44 @@
+import type { Nutritions, Fruit } from '../components/common.types'
+import axios from 'axios';
+
+//response from API is and array of objects so we need to specify its type.
+//Due to Fruit is and object, APIResponse would be Array<Fruit>.
+
+type APIResponse = Array<Fruit>;
+
+
+const fruitsURL = "https://proxyserver-phi.vercel.app"
+const fruitByIdURL = (id: number) => `https://proxyserver-phi.vercel.app/showd/${id}`
+
+//con axios
+const getDataFromApi = async (): Promise<APIResponse> => { //Promise is built in Typescript
+
+    try {
+        const response: APIResponse = await axios.get(fruitsURL); //axios.get() starts a request and returns a promise
+        return response
+
+    } catch (error) {
+        console.log(error)
+        throw new Error("There was an error on request or process data") //new Error(): Explicitly creates a new instance of Error, providing greater clarity and adherence to standard JavaScript conventions.
+    }
+
+}
+
+
+const getDataByIdFromApi = async (id: number) => {
+    try {
+        const response = await axios.get<Fruit>(fruitByIdURL(id)); //As the expected answer is Fruit Type
+
+        if (!response) { return null }
+
+        return response.data; //When we analyze Api response in console, we can see that its response is inside "data" property
+
+    } catch (error) {
+        throw Error("There was an error on request or process data")
+    }
+
+
+};
+
+export { getDataFromApi, getDataByIdFromApi }
+
