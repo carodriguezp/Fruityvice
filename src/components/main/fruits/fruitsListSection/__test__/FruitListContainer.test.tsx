5b62c693c521ace006bfb21c93f861f7e3b917ea
carodriguezp <crpzgz92@gmail.com>
2024-09-01 18:19:04 +0200

    fruitlist test and fruitlistcontainer test

--- src/components/main/fruits/fruitsListSection/__test__/FruitListContainer.test.tsx
+++ src/components/main/fruits/fruitsListSection/__test__/FruitListContainer.test.tsx
@@ -1,18 +1,22 @@
+import * as myService from "../../../../../services/api";
 import { mockFruitListFromApi } from "../../../../mocks";
-import FruitList from "../FruitList";
 import { render, screen } from "@testing-library/react";
 
 import "@testing-library/jest-dom";
-import { HashRouter } from "react-router-dom";
+import FruitListContainer from "../FruitListContainer";
+
+jest.mock("../../../../../services/api"); //route to be mocked
 
 describe("FruitList section calls API services", () => {
   describe("when succesfull", () => {
+    beforeEach(() => {
+      jest
+        .spyOn(myService, "getDataFromApi")
+        .mockResolvedValue(mockFruitListFromApi);
+    });
+
     it("renders FruitList section", async () => {
-      render(
-        <HashRouter>
-          <FruitList fruitsList={mockFruitListFromApi} />
-        </HashRouter>
-      );
+      render(<FruitListContainer />);
 
       const list = screen.getByTestId("list-of-fruits");
       expect(list).toBeInTheDocument();
@@ -20,8 +24,12 @@ describe("FruitList section calls API services", () => {
   });
 
   describe("when not succesfull", () => {
+    beforeEach(() => {
+      jest.spyOn(myService, "getDataFromApi").mockResolvedValue([]);
+    });
+
     it("throws an error", async () => {
-      render(<FruitList fruitsList={[]} />);
+      render(<FruitListContainer />);
 
       const errorMessage = await screen.findByTestId("error-message");
 
